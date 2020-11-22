e7cb7c7a014dac9cf57dee85b3bebeed6c4e089f
villalbapaezmiguel <villalbapaezmiguel@gmail.com>
2020-11-21 22:08:57 -0300

    add env with movie api key

--- src/api/APIMovie.js
+++ src/api/APIMovie.js
@@ -1,6 +1,6 @@
 const getMovieRequest = async (searchValue, setMovies) => {
     if (searchValue && searchValue.length >= 3) {
-        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=5aa91a63`;
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
         const response = await fetch(url);
         const responseJson = await response.json();
